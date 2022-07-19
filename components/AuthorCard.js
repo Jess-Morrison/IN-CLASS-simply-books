5ea218ee5b5ec1765fb20b26a522b79011546118
Jessica Morrison <jnicomorr@gmail.com>
2022-07-18 22:29:03 -0500

    Fix authors page

--- components/AuthorCard.js
+++ components/AuthorCard.js
@@ -1,39 +1,43 @@
+/* eslint-disable camelcase */
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function AuthorCard({
-  email, firebaseKey, firstName, lastName, favorite, image,
+
+  email, firebaseKey, first_name, last_name, favorite, image,
 }) {
   return (
     <>
       <div>Author </div>
       <div> Email: {email}</div>
       <div> FirebaseKey: {firebaseKey}</div>
-      <div> First Name: {firstName}</div>
-      <div> Last Name: {lastName}</div>
+      <div> First Name: {first_name}</div>
+      <div> Last Name: {last_name}</div>
       <div> Favorite : {favorite}</div>
-      <img src={image} alt={firstName} width="300" height="300" />
+      <img src={image} alt={first_name} width="200" height="200" />
     </>
   );
 }
 
 AuthorCard.propTypes = {
-  email: PropTypes.string,
-  firebaseKey: PropTypes.string,
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  image: PropTypes.string,
-  favorite: PropTypes.bool,
+  // authorObj: PropTypes.shape({
+  email: PropTypes.string.isRequired,
+  favorite: PropTypes.bool.isRequired,
+  firebaseKey: PropTypes.string.isRequired,
+  first_name: PropTypes.string.isRequired,
+  last_name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
 
+  // }).isRequired,
 };
 
-AuthorCard.defaultProps = {
-  email: 'ta@ta.com',
-  firebaseKey: '-MiBsfuTafbEQ7eAULxV',
-  firstName: 'Tomi',
-  lastName: 'Adeyemi',
-  image: 'https://images-na.ssl-images-amazon.com/images/I/A1agLFsWkOL.jpg',
-  favorite: 'True',
+// AuthorCard.defaultProps = {
+//   email: 'ta@ta.com',
+//   firebaseKey: '-MiBsfuTafbEQ7eAULxV',
+//   firstName: 'Tomi',
+//   lastName: 'Adeyemi',
+//   favorite: true,
+//   image: 'https://images-na.ssl-images-amazon.com/images/I/A1agLFsWkOL.jpg',
 
-};
+// };
--- pages/author.js
+++ pages/author.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
 import AuthorCard from '../components/AuthorCard';
+import { useAuth } from '../utils/context/authContext';
+import { getAuthors } from '../api/authorData';
 
 export default function Author() {
+  const { user } = useAuth();
+
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => { getAuthors(user.uid).then(setAuthors); }, [user.uid]);
+
   return (
-    <>
+    <div>
+      <Link href="/author/new" passHref>
+        <Button>Add An Author</Button>
+      </Link>
+      <div>
+        { authors.map((author) => (<AuthorCard key={author.firebaseKey} />))}
+      </div>
 
-      <AuthorCard />
-
-    </>
+    </div>
   );
 }
